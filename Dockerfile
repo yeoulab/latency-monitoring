56ef91a0b66a342415ab4273dca658348948b053
root <root@localhost.localdomain>
2021-07-10 16:59:48 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM docker.elastic.co/beats/heartbeat:7.12.0
+
+WORKDIR /usr/share/heartbeat
+
+COPY certs ./certs
+COPY heartbeat.docker.yml ./
+COPY heartbeat.policy.json ./
+
+CMD ["./heartbeat", "-e"]
---
+++ run_7.12.0.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+docker run \
+  --rm \
+  -d \
+  --name=heartbeat \
+  --user=heartbeat \
+  --net=host \
+  --volume="$(pwd)/heartbeat.docker.7.12.0.yml:/usr/share/heartbeat/heartbeat.yml:ro" \
+  --volume="/root/tmp_7.12.0/cert_blog/certs:/usr/share/heartbeat/certs:ro" \
+  --volume="$(pwd)/heartbeat.policy.json:/usr/share/heartbeat/heartbeat.policy.json" \
+  docker.elastic.co/beats/heartbeat:7.12.0 heartbeat -e\
